f8860bce0385f7fa29d51a5032377dc287ec7ba7
h950203 <h950203@gmail.com>
2026-01-20 16:02:53 +0900

    0120 1602

--- use_curator_v1.1.py
+++ use_curator_v1.1.py
@@ -5,7 +5,7 @@ from nemo_curator.pipeline import Pipeline
 from nemo_curator.stages.text.io.reader import JsonlReader
 from nemo_curator.stages.text.io.writer import JsonlWriter
 from nemo_curator.stages.text.filters.custom_filter import MultiLingualQualityFilterStage
-from nemo_curator.stages.text.quality.utils import preprocess_text
+from nemo_curator.stages.text.utils.custom_utils import preprocess_text
 from nemo_curator.stages.text.preprocessing.korean_preprocessing import KoreanPersonalFilter
 from nemo_curator.stages.text.preprocessing.english_preprocessing import EnglishPersonalFilter
 from nemo_curator.stages.text.preprocessing.japanese_preprocessing import JapanesePersonalFilter
